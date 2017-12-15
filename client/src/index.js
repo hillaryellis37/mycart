fcb8697db366148a7c0525c7a76abb55a588f068
raywhui <rwhui.tech@gmail.com>
2017-12-15 14:47:31 -0800

    FB login

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import FacebookLogin from 'react-facebook-login';
+
+const responseFacebook = (response) => {
+  console.log(response);
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// ReactDOM.render(
+//   <FacebookLogin
+//     appId="1088597931155576"
+//     autoLoad={true}
+//     fields="name,email,picture"
+//     onClick={console.log('fb')}
+//     callback={responseFacebook} 
+//     icon="fa-facebook"
+//   />,
+//   document.getElementById('demo')
+// );
