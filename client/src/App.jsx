70ece71fc4806ea8600f44084b40a25aa1482ee7
raywhui <rwhui.tech@gmail.com>
2017-12-18 18:38:13 -0800

    puspushing to master

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import FacebookLogin from 'react-facebook-login';
 import NavBar from "./components/Navbar";
 import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
